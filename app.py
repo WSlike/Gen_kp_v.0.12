3d486476b73b326fb7ed4c4c987e1a84978d7929
Slike <wslike@yandex.ru>
2019-04-20 23:00:59 +0300

    20190420_3

--- app.py
+++ app.py
@@ -1,27 +1,71 @@
-from extended_minidom import create_tag
+from extended_minidom import create_tag, create_var
+from datetime import datetime
+
 
 doc = create_tag()
+print(doc)
+project = create_tag('project', attributes={'xmlns': 'http://www.plcopen.org/xml/tc6_0200'})
+fileHeader = create_tag('fileHeader', attributes={'companyName': '',
+                                                  'productName': 'CODESYS',
+                                                  'productVersion': 'CODESYS V3.5 SP7 Patch 4',
+                                                  'creationDateTime': str(datetime.now())})
+contentHeader = create_tag('contentHeader', attributes={'name': 'kp15_20190419.project',
+                                                        'modificationDateTime': str(datetime.now())})
 
-root = create_tag('root')
-doc.appendChild(root)
+coordinateInfo = create_tag('coordinateInfo')
+fbd = create_tag('fbd')
+ld = create_tag('ld')
+sfc = create_tag('sfc')
+scaling_fbd = create_tag('scaling', attributes={'x': '1', 'y': '1'})
+scaling_ld = create_tag('scaling', attributes={'x': '1', 'y': '1'})
+scaling_sfc = create_tag('scaling', attributes={'x': '1', 'y': '1'})
+addData_ProjectInformation = create_tag('addData')
+data_ProjectInformation = create_tag('data',
+                                     attributes={'name': 'http://www.3s-software.com/plcopenxml/projectinformation',
+                                                 'handleUnknown': 'implementation'})
+ProjectInformation = create_tag('ProjectInformation')
+types = create_tag('types')
+dataTypes = create_tag('dataTypes')
+pous = create_tag('pous')
+pou = create_tag('pou', attributes={'name': 'POU',
+                                    'pouType': 'program'})
+interface = create_tag('interface')
+localVars = create_tag('localVars')
+
+doc.appendChild(project)
+project.appendChild(fileHeader)
+project.appendChild(contentHeader)
+contentHeader.appendChild(coordinateInfo)
+coordinateInfo.appendChild(fbd)
+fbd.appendChild(scaling_fbd)
+coordinateInfo.appendChild(ld)
+ld.appendChild(scaling_ld)
+coordinateInfo.appendChild(sfc)
+sfc.appendChild(scaling_sfc)
+contentHeader.appendChild(addData_ProjectInformation)
+addData_ProjectInformation.appendChild(data_ProjectInformation)
+data_ProjectInformation.appendChild(ProjectInformation)
+project.appendChild(types)
+types.appendChild(dataTypes)
+types.appendChild(pous)
+pous.appendChild(pou)
+
+pou.appendChild(interface)
+interface.appendChild(localVars)
+create_var(localVars, var_name='state', var_type='INT', var_simple_value='0')
+create_var(localVars, var_name='i', var_type='WORD')
+
 
 addData = create_tag('addData')
-data = create_tag('data', '', {'name': 'http://www.3s-software.com/plcopenxml/method', 'handleUnknown': 'implementation'})
-Method = create_tag('Method', '', {'name': 'IEC104_Init', 'ObjectId': 'IEC104_Init'})
+data = create_tag('data', attributes={'name': 'http://www.3s-software.com/plcopenxml/method',
+                                      'handleUnknown': 'implementation'})
+Method = create_tag('Method', attributes={'name': 'IEC104_Init', 'ObjectId': 'IEC104_Init'})
 interface = create_tag('interface')
 body = create_tag('body')
 ST = create_tag('ST')
-xhtml = create_tag('xhtml', '//Настройки Slave IEC104', {'xmlns': 'http://www.w3.org/1999/xhtml'})
-root.appendChild(addData)
-addData.appendChild(data)
-data.appendChild(Method)
-Method.appendChild(interface)
-Method.appendChild(body)
-body.appendChild(ST)
-ST.appendChild(xhtml)
+xhtml = create_tag('xhtml', '//Настройки Slave IEC104', attributes={'xmlns': 'http://www.w3.org/1999/xhtml'})
 
+xml_str = doc.toprettyxml(indent="  ", encoding='utf-8')
 
-
-xml_str = doc.toprettyxml(indent="  ")
-with open("minidom_example2.xml", "w") as f:
+with open("minidom_example2.xml", "wb") as f:
     f.write(xml_str)
--- extended_minidom.py
+++ extended_minidom.py
@@ -20,3 +20,18 @@ def create_tag(name: str = None, text: str = None, attributes: dict = None, *, c
             tag.setAttribute(k, str(v))
 
     return tag
+
+
+def create_var(root: minidom.Document, var_name: str = None, var_type: str = None, var_simple_value: str = None):
+    tag_variable = create_tag('variable', attributes={'name': str(var_name)})
+    tag_type = create_tag('type')
+    tag_type_init = create_tag(str(var_type))
+    tag_initial_value = create_tag('initialValue')
+    tag_simple_value = create_tag('simpleValue', attributes={'value': str(var_simple_value)})
+    root.appendChild(tag_variable)
+    tag_variable.appendChild(tag_type)
+    tag_type.appendChild(tag_type_init)
+
+    if var_simple_value is not None:
+        tag_variable.appendChild(tag_initial_value)
+        tag_initial_value.appendChild(tag_simple_value)
