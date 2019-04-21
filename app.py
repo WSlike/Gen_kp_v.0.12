e9880bcede58afd85733ad26676b2d01b29bc88b
Slike <wslike@yandex.ru>
2019-04-21 16:55:40 +0300

    20190421

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from extended_minidom import create_tag, create_var
 from datetime import datetime
 
-
 doc = create_tag()
-print(doc)
 project = create_tag('project', attributes={'xmlns': 'http://www.plcopen.org/xml/tc6_0200'})
 fileHeader = create_tag('fileHeader', attributes={'companyName': '',
                                                   'productName': 'CODESYS',
@@ -52,20 +50,103 @@ pous.appendChild(pou)
 
 pou.appendChild(interface)
 interface.appendChild(localVars)
-create_var(localVars, var_name='state', var_type='INT', var_simple_value='0')
-create_var(localVars, var_name='i', var_type='WORD')
+create_var(localVars, var_name='state', var_type='INT ', var_simple_value='0')
+create_var(localVars, var_name='i', var_type='WORD ')
+create_var(localVars, var_name='j', var_type='WORD ')
+create_var(localVars, var_name='res_fram_write', var_type='INT ', comment='результат записи 0 - всё хорошо			')
+create_var(localVars, var_name='crc_fram_read', var_type='INT ', comment='посчитанный CRC из памяти')
+create_var(localVars, var_name='srv_104', var_type='RLTNU.IEC104Server ', derived=True, comment=' IEC 104')
+create_var(localVars, var_name='db_104', var_type='RLTNU.IECDB ', derived=True)
+create_var(localVars, var_name='ts_104', var_type='RLTNU.IEC_M_SP_NA ', derived=True, array=['1', 'q_ts_full'],
+           comment='мой массив бит на передачу в 104')
+create_var(localVars, var_name='tf_104', var_type='RLTNU.IEC_M_ME_NC ', derived=True, array=['1', '(2 * q_ti_full)'],
+           comment='мой массив float на передачу в 104')
+create_var(localVars, var_name='tu_104', var_type='RLTNU.IECCommand ', derived=True, array=['1', 'q_tu_full'],
+           comment=' массив команд в 104')
+create_var(localVars, var_name='trf_104', var_type='RLTNU.IECCommand ', derived=True, array=['1', 'q_tr_full'],
+           comment=' массив ТР в 104')
+create_var(localVars, var_name='cmd_104', var_type='RLTNU.IECCommand ', derived=True)
+create_var(localVars, var_name='event', var_type='RLTNU.IECSpontaneousEvent ', derived=True)
+create_var(localVars, var_name='sys_tick', var_type='CAA.TICK ', derived=True, comment=' Время')
+create_var(localVars, var_name='sys_tick_bool', var_type='BOOL ')
+create_var(localVars, var_name='current_time', var_type='UDINT ')
+create_var(localVars, var_name='hiTime', var_type='ULINT ')
+create_var(localVars, var_name='Start_timer', var_type='TON', derived=True, comment=' Таймер первого запуска')
+create_var(localVars, var_name='First_start', var_type='BOOL ', comment=' Флаг первого запуска')
 
-
-addData = create_tag('addData')
-data = create_tag('data', attributes={'name': 'http://www.3s-software.com/plcopenxml/method',
-                                      'handleUnknown': 'implementation'})
-Method = create_tag('Method', attributes={'name': 'IEC104_Init', 'ObjectId': 'IEC104_Init'})
-interface = create_tag('interface')
 body = create_tag('body')
 ST = create_tag('ST')
-xhtml = create_tag('xhtml', '//Настройки Slave IEC104', attributes={'xmlns': 'http://www.w3.org/1999/xhtml'})
+xhtml = create_tag('xhtml', attributes={'xmlns': 'http://www.w3.org/1999/xhtml'})
+ST_code = '''// Begin
+sys_tick := TICKS.GetTick(sys_tick_bool);
+current_time := CAA.TICK_TO_UDINT(sys_tick);
+SysTimeRtc.SysTimeRtcHighResGet(hiTime);
+
+CASE state OF
+	0:
+		////Настройка IEC104	//
+		POU.IEC104_Init();
+
+		////Работа с ТУ	//
+		POU.TU_Init();
+		
+		////Работа с ТС//
+		POU.TS_Init();
+		
+		////Работа с ТИ//		
+		POU.TI_Init();
+		
+		First_start := TRUE;				// Поднимаем флаг первого запуска	
+		state := 200;
+		
+	200:
+
+
+		////Первый запуск//			
+		Start_Timer( IN:=TRUE, PT:=T#5S ); // Запуск Таймера первого запуска
+
+		
+		////Запуск передачи данных IEC104//		
+		srv_104();
+		IF srv_104.error THEN
+			state := 1000;
+		END_IF
+		
+		/////Обработка и передача ТC
+		POU.TS_Work(); 
+		
+		/////Обработка и передача ТИ
+		POU.TI_Work();
+		
+		//// Индикация на шкафу
+		POU.Valve_Indication();
+		POU.Pump_Indication();
+		
+		///Обработка ТУ и ТР
+		POU.TU_TR_WORK();
+
+		
+		
+		ModbusMaster_SPort.enable := TRUE;	
+		ModbusMaster_SPort();
+		ModbusMaster_VPort.enable := TRUE;	
+		ModbusMaster_VPort();
+		
+		/////Сброс флага первого запуска
+		IF Start_Timer.Q THEN
+			First_start := FALSE;
+		END_IF
+		
+		state := 200;
+	
+	1000:
+		srv_104.reset();
+		state := 0;
+		
+END_CASE
+
+// End'''
 
 xml_str = doc.toprettyxml(indent="  ", encoding='utf-8')
-
 with open("minidom_example2.xml", "wb") as f:
     f.write(xml_str)
--- extended_minidom.py
+++ extended_minidom.py
@@ -22,16 +22,41 @@ def create_tag(name: str = None, text: str = None, attributes: dict = None, *, c
     return tag
 
 
-def create_var(root: minidom.Document, var_name: str = None, var_type: str = None, var_simple_value: str = None):
+def create_var(root: minidom.Document, var_name: str = None, var_type: str = None, var_simple_value: str = None,
+               comment: str = None, derived: bool = False, array: list = None):
     tag_variable = create_tag('variable', attributes={'name': str(var_name)})
     tag_type = create_tag('type')
     tag_type_init = create_tag(str(var_type))
     tag_initial_value = create_tag('initialValue')
     tag_simple_value = create_tag('simpleValue', attributes={'value': str(var_simple_value)})
+    tag_documentation = create_tag('documentation')
+    tag_xhtml = create_tag('xhtml', str(comment), attributes={'xmlns': 'http://www.w3.org/1999/xhtml'})
+    tag_derived = create_tag('derived', attributes={'name': str(var_type)})
+
     root.appendChild(tag_variable)
     tag_variable.appendChild(tag_type)
-    tag_type.appendChild(tag_type_init)
+
+    if array is not None:
+        tag_array = create_tag('array')
+        tag_dimension = create_tag('dimension', attributes={'lower': str(array[0]), 'upper': str(array[1])})
+        tag_base_type = create_tag('baseType')
+        tag_type.appendChild(tag_array)
+        tag_array.appendChild(tag_dimension)
+        tag_array.appendChild(tag_base_type)
+        if derived:
+            tag_base_type.appendChild(tag_derived)
+        else:
+            tag_base_type.appendChild(tag_type_init)
+    else:
+        if derived:
+            tag_type.appendChild(tag_derived)
+        else:
+            tag_type.appendChild(tag_type_init)
 
     if var_simple_value is not None:
         tag_variable.appendChild(tag_initial_value)
         tag_initial_value.appendChild(tag_simple_value)
+
+    if comment is not None:
+        tag_variable.appendChild(tag_documentation)
+        tag_documentation.appendChild(tag_xhtml)
